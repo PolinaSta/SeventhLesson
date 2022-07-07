820379c71f0f8e4467f528221f66451769b13bac
Polina <polstaver@yandex.by>
2022-07-07 17:58:48 +0300

    сохранение ДЗ

--- Program.cs
+++ Program.cs
@@ -111,7 +111,6 @@
 //                 array[i, j] *= array[i, j];
 //         }
 //     }
-
 // }
 
 // Console.WriteLine("Enter number of rows: ");
@@ -149,9 +148,9 @@
 //     }
 //     return array;
 // }
+
 // void ShowArray(int[,] array)
 // {
-
 //     for (int i = 0; i < array.GetLength(0); i++)
 //     {
 //         for (int j = 0; j < array.GetLength(1); j++)
@@ -161,6 +160,7 @@
 //     Console.WriteLine();
 //     }
 // }
+
 // int SumNumber(int[,] array)
 // {
 //     int sum = 0;
@@ -191,3 +191,157 @@
 // Console.WriteLine(SumNumber(myArray));
 
 
+// ДОМАШНЕЕ ЗАДАНИЕ
+
+// 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// void Main(string[] args) // Метод создания массива с рандомными дробными числами
+// {
+// Console.WriteLine("Введите количество строк массива и столбцов последовательно");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+//     Random x = new Random(); // объявление переменной для генерации чисел
+//     double[,] masD = new double[rows, columns];
+
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//             masD[i, j] = Convert.ToDouble(x.Next(-100, 100) / 10.0); // случайные числа от -10 до 9.9!!!
+//     }
+//     Console.WriteLine("\nРандомный массив:");
+//     for (int i = 0; i < rows; i++)
+//     {
+//         Console.WriteLine();
+//         for (int j = 0; j < columns; j++)
+//             Console.Write("\t" + masD[i, j]);
+//     }
+// }
+// Main(args);
+
+// 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+
+// Console.Write("Enter number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// int[,] array = CreateRandomArray(rows,columns);
+// ShowArray(array);
+
+// Console.Write("Enter number of columns: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(SearchNum(array, number))
+// {
+//     Console.WriteLine("Элемент найден");
+// }
+// else
+// {
+//     Console.WriteLine("Элемент не найден");
+// }
+
+// int[,] CreateRandomArray(int m, int n)
+// {
+//     int[,] array = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             array[i, j] = new Random().Next(1,10);
+//         }
+//     }
+//     return array;
+// }
+
+// void ShowArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j] + " ");
+//         }
+//     Console.WriteLine();
+//     }
+// }
+
+// bool SearchNum (int [,] array, int number)
+// {
+//  for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if(array[i,j] == number) return true;
+//         }
+//     }
+//     return false;
+// }
+
+
+// 3.  Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// Console.Write("Enter number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] array = CreateRandomArray(rows,columns);
+// ShowArray(array);
+
+// double [] averageColumns = GetResultArray(array);
+// Console.WriteLine($"Среднее арифметическое каждого столбца = {String.Join("; ",averageColumns)}");
+
+// int[,] CreateRandomArray(int m, int n)
+// {
+//     int[,] array = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             array[i, j] = new Random().Next(1,10);
+//         }
+//     }
+//     return array;
+// }
+
+// void ShowArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j] + " ");
+//         }
+//     Console.WriteLine();
+//     }
+// }
+
+// double [] GetResultArray(int[,] array)
+// {
+//     double[] result = new double[array.GetLength(1)];
+//     for (int column = 0; column<array.GetLength(1); column++)
+//     {
+//         double sum=0;
+//         for (int row = 0; row<array.GetLength(0); row++)
+//         {
+//             sum += array[row,column];
+//         }
+//         result[column] = Math.Round(sum/array.GetLength(0),2);
+//     }
+//     return result;
+// }
